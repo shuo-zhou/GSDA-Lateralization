4e0842cee8c6d910214285c72d15ff84aa38bef9
Shuo Zhou <szhou20@sheffield.ac.uk>
2021-12-16 22:11:13 +0000

    Update main_sub_half.py

--- main_sub_half.py
+++ main_sub_half.py
@@ -31,7 +31,7 @@ def main():
     # lambdas = [0.1, 0.5]  # [0.0, 1.0, 2.0, 5.0, 8.0, 10.0]
     lambdas = [0.0, 1.0, 2.0, 5.0, 8.0, 10.0]
     # lambdas = [10.0]
-    l2_param = 0.1
+    l2_param = 10
     test_sizes = [0.1, 0.2, 0.3, 0.4]
 
     data = dict()
@@ -92,8 +92,8 @@ def main():
                 y_train = y_all[train_session][i_fold]
                 x_test_ = x_all[train_session][1 - i_fold]
                 y_test_ = y_all[train_session][1 - i_fold]
-                scaler = StandardScaler()
-                scaler.fit(x_train)
+                # scaler = StandardScaler()
+                # scaler.fit(x_train)
                 for train_gender in [0, 1]:
                     ic_is_idx = np.where(genders_train == train_gender)[0]
                     oc_is_idx = np.where(genders_train == 1 - train_gender)[0]
@@ -118,28 +118,29 @@ def main():
                                "acc_oc_is": [x_test_oc_is, y_test_oc_is], "acc_oc_os": [x_test_oc_os, y_test_oc_os]}
 
                     for lambda_ in lambdas:
-                        model = CoDeLR(lambda_=lambda_, l2_hparam=l2_param)
+                        # model = CoDeLR(lambda_=lambda_, l2_hparam=l2_param)
                         # model = CoDeLogitReg(regularization=None, lambda_=lambda_, C=l2_param, max_iter=2000)
-                        # model = CoDeLogitReg(lambda_=lambda_, C=10, max_iter=5000)
+                        model = CoDeLogitReg(lambda_=lambda_, C=l2_param, max_iter=5000)
                         model_path = os.path.join(out_dir, "lambda_%s_%s_%s_%s_gender_%s.pt" %
                                                   (lambda_, train_session, i_split, i_fold, train_gender))
                         if os.path.exists(model_path):
                             model = torch.load(model_path)
                         else:
-                            model.fit(torch.from_numpy(scaler.transform(x_train)).float(),
-                                      y_train[ic_is_idx], genders_train, target_idx=ic_is_idx)
+                            # model.fit(scaler.transform(x_train), y_train[ic_is_idx], genders_train,
+                            #           target_idx=ic_is_idx)
+                            model.fit(x_train, y_train[ic_is_idx], genders_train, target_idx=ic_is_idx)
                             # model.fit(x_train, y_train[ic_is_idx], genders_train, target_idx=ic_is_idx)
                             torch.save(model, model_path)
 
                         for acc_key in xy_test:
                             test_x, test_y = xy_test[acc_key]
                             # y_pred_ = model.predict(test_x)
-                            y_pred_ = model.predict(torch.from_numpy(scaler.transform(test_x)).float())
-                            acc_ = accuracy(test_y, y_pred_.view(-1).int())
-                            res[acc_key].append(acc_.item())
+                            # acc_ = accuracy(test_y, y_pred_.view(-1).int())
+                            # res[acc_key].append(acc_.item())
                             # y_pred_ = model.predict(scaler.transform(test_x))
-                            # acc_ = accuracy_score(test_y, y_pred_)
-                            # res[acc_key].append(acc_)
+                            y_pred_ = model.predict(test_x)
+                            acc_ = accuracy_score(test_y, y_pred_)
+                            res[acc_key].append(acc_)
 
                         res['pred_loss'].append(model.losses['pred'][-1])
                         res['code_loss'].append(model.losses['code'][-1])
