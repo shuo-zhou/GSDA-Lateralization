c94204d8b755e644fd67d09aba133fa7aa6e2191
Shuo Zhou <szhou20@sheffield.ac.uk>
2022-01-02 16:49:27 +0000

    Update cr8_cfgs.py

--- configs/cr8_cfgs.py
+++ configs/cr8_cfgs.py
@@ -9,7 +9,7 @@ def mk_dir(dir_):
 
 py_dir = "/shared/tale2/Shared/szhou/code/Brain_LR_BNU"
 batch_dir = "/shared/tale2/Shared/szhou/qsub/Brain_LR"
-py_file = "main.py"
+py_file = "main_hpc.py"
 seeds = 2022 - np.arange(50)
 lambdas = [0.0, 1.0, 2.0, 5.0, 8.0, 10.0]
 
