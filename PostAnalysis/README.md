120b073e5c725aa1adc1d574cd571548251339d6
shuo-zhou <sz144@outlook.com>
2023-07-03 17:25:44 +0100

    update res on HCP, GSP and ukb

---
+++ PostAnalysis/README.md
@@ -0,0 +1,16 @@
+# The script for the chord plot and spec_rate
+
+## Meeting notes (03.07.2023)
+
+- [] correlation between univariate models and multivariate models:
+  - [] univariate control vs multivariate control
+  - [] univariate male vs multivariate male
+  - [] univariate female vs multivariate female
+
+- [] core results of laterality patterns:
+  - [] univariate: 并集 - 交集
+  - [] multivariate: mask generated from second-order coefficients
+
+Remove UK-biobank results
+
+No need to quantify laterality patterns unless there are conclusions to be made about.
--- model_stats/first_order_coef_analysis.py
+++ model_stats/first_order_coef_analysis.py
@@ -6,22 +6,23 @@ from second_order_clf import fetch_weights
 
 
 def main():
-    dataset = "gsp"
-    # base_dir = "/media/shuo/MyDrive/data/brain/brain_networks/ukbio/Models"
-    base_dir = "/media/shuo/MyDrive/data/brain/brain_networks/gsp/Models"
+    # dataset = "gsp"
+    # base_dir = "/media/shuo/MyDrive/data/brain/brain_networks/gsp/Models"
+    dataset = "ukb"
+    base_dir = "/media/shuo/MyDrive/data/brain/brain_networks/ukbio/Models"
     # sessions = ["REST1_", "REST2_"]
     sessions = [""]
-    seed_ =2023
+    seed_ = 2023
     # for lambda_ in [0, 2]:
-    # for lambda_ in ["0_mix_gender"]:
-    #     for gender in ["mix"]:
-    for lambda_ in [0, 2, 5]:
-        for gender in [0, 1]:
+    for lambda_ in ["0_mix_gender"]:
+        for gender in ["mix"]:
+    # for lambda_ in [0, 2, 5]:
+    #     for gender in [0, 1]:
             weight = fetch_weights(base_dir, gender, lambda_, dataset, sessions=sessions, seed_=seed_)
             w_mean = np.mean(weight, axis=0)
             w_std = np.std(weight, axis=0)
             midc = {"mean": w_mean, "std": w_std}
-            fname = "L%sG%s.mat" % (lambda_, gender)
+            fname = "%s_L%sG%s" % (dataset, lambda_, gender)
             savemat(os.path.join(base_dir, fname + ".mat"), midc)
 
 
