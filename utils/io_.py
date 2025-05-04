a71121c259b8cda9b59287edf90e6253e22f346e
Shuo Zhou <shuo.zhou@sheffield.ac.uk>
2025-05-04 13:52:01 +0100

    update plot cls results

--- utils/io_.py
+++ utils/io_.py
@@ -271,7 +271,9 @@ def load_half_brain(
     return data
 
 
-def fetch_weights(base_dir, group, lambda_, dataset, sessions, test_size="00", seed_=2023):
+def fetch_weights(
+    base_dir, group, lambda_, dataset, sessions, test_size="00", seed_=2023
+):
     """
 
     Args:
@@ -311,7 +313,9 @@ def fetch_weights(base_dir, group, lambda_, dataset, sessions, test_size="00", s
                         seed_ - seed,
                     )
                     if os.path.exists(os.path.join(sub_dir, model_file_name)):
-                        weight.append(get_coef(model_file_name, sub_dir).reshape((1, -1)))
+                        weight.append(
+                            get_coef(model_file_name, sub_dir).reshape((1, -1))
+                        )
 
     return np.concatenate(weight, axis=0)
 
@@ -377,106 +381,6 @@ def save_results(res_dict, out_filename, output_dir, mix_group=False):
     res_df.to_csv(out_file, index=False)
 
 
-def load_result(dataset, root_dir, lambdas, seed_start, test_size=0.0):
-    """load brain left/right classification results for a dataset
-
-    Args:
-        dataset (string): _description_
-        root_dir (string): _description_
-        lambdas (list): _description_
-        seed_start (_type_): _description_
-        test_size (float, optional): _description_. Defaults to 0.0.
-    """
-    res_dict = dict()
-    res_list = []
-    test_size_str = str(int(test_size * 10))
-    for lambda_ in lambdas:
-        res_dict[lambda_] = []
-
-    for lambda_ in lambdas:
-        if not isinstance(lambda_, str):
-            lambda_str = str(int(lambda_))
-        else:
-            lambda_str = lambda_
-        model_dir = os.path.join(root_dir, "lambda%s" % lambda_str)
-        for seed_iter in range(51):
-            random_state = seed_start - seed_iter
-            res_fname = "results_%s_L%s_test_size0%s_Fisherz_%s.csv" % (
-                dataset,
-                lambda_str,
-                test_size_str,
-                random_state,
-            )
-            res_fpath = os.path.join(model_dir, res_fname)
-            if os.path.exists(res_fpath):
-                res_df = pd.read_csv(os.path.join(model_dir, res_fname))
-                res_df["seed"] = random_state
-                res_dict[lambda_].append(res_df)
-                res_list.append(res_df)
-
-    for lambda_ in lambdas:
-        res_dict[lambda_] = pd.concat(res_dict[lambda_])
-
-    res_df_all = pd.concat(res_list)
-    res_df_all = res_df_all.reset_index(drop=True)
-
-    return res_df_all
-
-
-def reformat_results(res_df, test_sets, male_label=0):
-    """reformat results dataframe to one accuracy per row
-
-    Args:
-        res_df (_type_): _description_
-        test_sets (_type_): _description_
-        male_label (int, optional): _description_. Defaults to 0.
-
-    Returns:
-        _type_: _description_
-    """
-    res_reformat = {
-        "Accuracy": [],
-        "Test set": [],
-        "Lambda": [],
-        "Target group": [],
-        "seed": [],
-        "split": [],
-        "fold": [],
-        "Train session": [],
-    }
-    for idx_ in res_df.index:
-        # print(idx_, res_df.iloc[idx_, 12])
-        subset_ = res_df.loc[idx_, :]
-        for test_set in test_sets:
-            res_reformat["Accuracy"].append(subset_[test_set])
-            res_reformat["Lambda"].append(subset_["lambda"])
-            if "train_session" in subset_:
-                res_reformat["Train session"].append(subset_["train_session"])
-            else:
-                res_reformat["Train session"].append(None)
-            if "target_group" not in subset_:  # for GSP dataset
-                _group = subset_["train_gender"]
-            else:
-                _group = subset_["target_group"]
-            test_set_list = test_set.split("_")
-            if _group == "Male" or _group == male_label:
-                res_reformat["Target group"].append("Male")
-                if "oc" in test_set_list or "tgt" in test_set_list:
-                    res_reformat["Test set"].append("Female")
-                else:
-                    res_reformat["Test set"].append("Male")
-            else:
-                res_reformat["Target group"].append("Female")
-                if "oc" in test_set_list or "tgt" in test_set_list:
-                    res_reformat["Test set"].append("Male")
-                else:
-                    res_reformat["Test set"].append("Female")
-
-            for key in ["seed", "split", "fold"]:
-                res_reformat[key].append(subset_[key])
-    return pd.DataFrame(res_reformat)
-
-
 # read: nib.load()
 # write: nib.save()
 # ********************************** file split ************************************* #
@@ -505,9 +409,7 @@ def read_file(file):
 
 def select_data_subj(df, subj_id, df_column_name):
     data = df[df_column_name][df["subject"] == subj_id]
-    return (
-        data  # series datatype with dim of (1,),for the ndarray contents, data = data[0]
-    )
+    return data  # series datatype with dim of (1,),for the ndarray contents, data = data[0]
 
 
 def pick_half(data, random_state=144):
