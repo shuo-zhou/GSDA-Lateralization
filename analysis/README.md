1342384390fb5518aa4404de7b68086fb04d5485
shuo-zhou~ <sz144@outlook.com>
2025-05-09 19:42:29 +0100

    rm post-analysis images

--- analysis/README.md
+++ analysis/README.md
@@ -1,4 +1,15 @@
-# Statistical analysis on 1st and 2nd-order coefficients
+# Statistical analysis on 1st and 2nd-order model weights and univariate test
+
+## Processing first-order model weights
+
+First-order models were trained to classify right (labelled as 1) and left (labelled as 0) brain hemispheres.
+
+`process_first_order_weights.py` processes first-order model weight (.pt) files by:
+
+1. Loading weight vectors learned from multiple random splits (seeds).
+2. Combining all weights into a single .npz file for archival and future use.
+3. Computing the mean and standard deviation across splits.
+4. Saving the aggregated results as a .mat file.
 
 Sample first-order (.pt) and second-order (.joblib) model files are available on Hugging Face Model Hub: [https://huggingface.co/shuo-zhou/brain_network_lateralization](https://huggingface.co/shuo-zhou/brain_network_lateralization), DOI: [10.57967/hf/2996](https://doi.org/10.57967/hf/2996).
 
@@ -6,17 +17,38 @@ Each `.mat` file contains the mean and standard deviation of coefficients for a 
 
 **Naming:** `L` denotes the $\lambda$, and `G` denotes the subjects' gender (0 for male and 1 for female) of the subset used for training.
 
-## First-order models
+## Processing second-order model weights
 
-The models were trained to classify right (positive class) and left brain (negative class).
+Second-order models were trained to classify two group of first-order models (male-specific vs female-specific).
 
-## Second-order models
+`process_second_order_weights.py` processes second-order model weight (.joblib) files by:
 
-The models were trained to classify two group of models.
+1. Loading weight vectors learned from multiple random splits (seeds).
+2. Computing the mean and standard deviation across splits.
+3. Saving the aggregated results as a .mat file.
 
-**Example**: "L2G0_vs_L2G1.mat"
+**Example output**: "L2G0_vs_L2G1.mat"
 
 - mean and standard deviation of models (logistic regression)to classify models trained respectively on males and females, both with $\lambda=2$
 - "L2G0" was labelled as class 1 and "L2G1" was labelled as class 0
 
 The output figures are available in the [figures](./figures) folder.
+
+## Univariate test and analysis
+
+- Plot the connectivity with significant weights as a chord diagram
+- Plot radar diagrams to represent the intra- and inter-lobe statistics of connections with significant weights
+
+**Input files:**
+
+Sample first-order (.pt) and second-order (.joblib) model files, along with statistical analysis results (.mat), are available on Hugging Face Model Hub: [https://huggingface.co/shuo-zhou/brain_network_lateralization](https://huggingface.co/shuo-zhou/brain_network_lateralization), DOI: [10.57967/hf/2996](https://doi.org/10.57967/hf/2996).
+
+The Brainnetome atlas (BNA) is available at [http://atlas.brainnetome.org/](http://atlas.brainnetome.org/). [BNA_subregions.xlsx](BNA_subregions.xlsx) for the information about the lobes and gyrus is provided by the author and originally available at [https://pan.cstcloud.cn/web/share.html?hash=6eRCJ0zDTFk](https://pan.cstcloud.cn/web/share.html?hash=6eRCJ0zDTFk).
+
+**Output files:**
+
+The output figures are available in the [figures](./figures) folder.
+
+Author: [Shuo Zhou](https://github.com/shuo-zhou) and [Junhao Luo](https://github.com/junhaols)
+
+Contact: <shuo.zhou@sheffield.ac.uk> adn <junhaol@mail.bnu.edu.cn>
--- analysis/plot_weight_corr_heatmanp.py
+++ analysis/plot_weight_corr_heatmanp.py
@@ -1,9 +1,10 @@
 import os
-from scipy.io import loadmat
-import pandas as pd
-import seaborn as sns
+
 import matplotlib.pylab as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from scipy.io import loadmat
 
 
 def load_weights_dict(base_dir, dataset, group_label, lambdas, test_size="00"):
@@ -14,6 +15,7 @@ def load_weights_dict(base_dir, dataset, group_label, lambdas, test_size="00"):
         fpath = os.path.join(base_dir, "first_order", fname)
         weights[key] = loadmat(fpath)["mean"][0][1:]
     return weights
+
 
 def plot_corr_heatmap(df, outfile, fontsize=14):
     corr = df.corr()
@@ -44,86 +46,31 @@ def plot_corr_heatmap(df, outfile, fontsize=14):
     plt.show()
 
 
-# def load_coef_plot_corr(dataset, group_label, test_size="00", fontsize=14):
-#     weights_dirs = dict()
-#     lambdas = [0.0, 1.0, 2.0, 5.0, 8.0, 10.0]
-#     sex_dict = {0: "Male", 1: "Female"}
-#     for lambda_ in lambdas:
-#         weights_dirs[r"$\lambda=%s$" % (int(lambda_))] = "%s/%s_L%sG%s_test_size%s.mat" % (
-#             dataset,
-#             dataset,
-#             int(lambda_),
-#             group_label,
-#             test_size
-#         )
-#
-#     weights = dict()
-#
-#     for key in weights_dirs:
-#         # print(key)
-#         weights[key] = loadmat(weights_dirs[key])["mean"][0][1:]
-#
-#     weight_df = pd.DataFrame(weights)
-#
-#     corr = weight_df.corr()
-#
-#     # Generate a mask for the upper triangle
-#     mask = np.triu(np.ones_like(corr, dtype=bool), 1)
-#
-#     plt.rcParams.update({"font.size": fontsize})
-#     # Set up the matplotlib figure
-#     f, ax = plt.subplots(figsize=(11, 9))
-#
-#     # Generate a custom diverging colormap
-#     cmap = sns.diverging_palette(230, 20, as_cmap=True)
-#     plt.rcParams["text.usetex"] = True
-#     # Draw the heatmap with the mask and correct aspect ratio
-#     sns.heatmap(
-#         corr,
-#         mask=mask,
-#         cmap=cmap,
-#         vmin=0,
-#         vmax=1,
-#         center=0.5,
-#         annot=True,
-#         annot_kws={"fontsize": "xx-large"},
-#         square=True,
-#         linewidths=0.5,
-#         cbar_kws={"shrink": 0.5},
-#     )
-#     # cbar_kws={"shrink": .5, "use_gridspec": False, "location": "top"})  #, annot_kws={"rotation": 45})
-#     plt.rcParams["text.usetex"] = False
-#     ax.set_xticklabels(weight_df.columns.to_list(), fontsize=fontsize + 2)
-#     ax.set_yticklabels(
-#         weight_df.columns.to_list(), rotation=45, ha="right", fontsize=fontsize + 2
-#     )
-#     # plt.savefig('corr.pdf', format='pdf', bbox_inches='tight')
-#     # plt.savefig('corr.png', format='png', bbox_inches='tight')
-#     plt.savefig(
-#         "figures/corr_annot_%s_%s.svg" % (dataset, sex_dict[group_label]),
-#         format="svg",
-#         bbox_inches="tight",
-#     )
-#     # plt.savefig('figures/corr_annot_%s_%s.pdf' % (dataset, sex_dict[sex_label]), format='pdf', bbox_inches='tight')
-#     # plt.savefig('figures/corr_annot_%s_%s.png' % (dataset, sex_dict[sex_label]), format='png', bbox_inches='tight')
-#     plt.show()
-
-
 def main():
     base_dir = "model_weights"
     out_fig_dir = "gigasci_figures"
     # fontsize = 14
     weights_dirs = {
-        "Multivariate Control HCP": "%s/first_order/HCP_L0_group_mixG0_test_size00.mat" % base_dir,
-        r"GSDA $\lambda=0$ Male HCP": "%s/first_order/HCP_L0G0_test_size00.mat" % base_dir,
-        r"GSDA $\lambda=0$ Female HCP": "%s/first_order/HCP_L0G1_test_size00.mat" % base_dir,
-        "Multivariate Control GSP": "%s/first_order/gsp_L0_group_mixG0_test_size00.mat" % base_dir,
-        r"GSDA $\lambda=0$ Male GSP": "%s/first_order/gsp_L0G0_test_size00.mat" % base_dir,
-        r"GSDA $\lambda=0$ Female GSP": "%s/first_order/gsp_L0G1_test_size00.mat" % base_dir,
-        r"GSDA $\lambda=5$ Male HCP": "%s/first_order/HCP_L5G0_test_size00.mat" % base_dir,
-        r"GSDA $\lambda=5$ Female HCP": "%s/first_order/HCP_L5G1_test_size00.mat" % base_dir,
-        r"GSDA $\lambda=5$ Male GSP": "%s/first_order/gsp_L5G0_test_size00.mat" % base_dir,
-        r"GSDA $\lambda=5$ Female GSP": "%s/first_order/gsp_L5G1_test_size00.mat" % base_dir,
+        "Multivariate Control HCP": "%s/first_order/HCP_L0_group_mixG0_test_size00.mat"
+        % base_dir,
+        r"GSDA $\lambda=0$ Male HCP": "%s/first_order/HCP_L0G0_test_size00.mat"
+        % base_dir,
+        r"GSDA $\lambda=0$ Female HCP": "%s/first_order/HCP_L0G1_test_size00.mat"
+        % base_dir,
+        "Multivariate Control GSP": "%s/first_order/gsp_L0_group_mixG0_test_size00.mat"
+        % base_dir,
+        r"GSDA $\lambda=0$ Male GSP": "%s/first_order/gsp_L0G0_test_size00.mat"
+        % base_dir,
+        r"GSDA $\lambda=0$ Female GSP": "%s/first_order/gsp_L0G1_test_size00.mat"
+        % base_dir,
+        r"GSDA $\lambda=5$ Male HCP": "%s/first_order/HCP_L5G0_test_size00.mat"
+        % base_dir,
+        r"GSDA $\lambda=5$ Female HCP": "%s/first_order/HCP_L5G1_test_size00.mat"
+        % base_dir,
+        r"GSDA $\lambda=5$ Male GSP": "%s/first_order/gsp_L5G0_test_size00.mat"
+        % base_dir,
+        r"GSDA $\lambda=5$ Female GSP": "%s/first_order/gsp_L5G1_test_size00.mat"
+        % base_dir,
     }
 
     weights = dict()
@@ -157,38 +104,6 @@ def main():
 
     weight_df = pd.concat((hcp_tval, gsp_tval, weight_df), axis=1)
 
-    # fontsize = 14
-    # corr = weight_df.corr()
-    #
-    # # Generate a mask for the upper triangle
-    # mask = np.triu(np.ones_like(corr, dtype=bool))
-    #
-    # # Set up the matplotlib figure
-    # f, ax = plt.subplots(figsize=(11, 9))
-    # plt.rcParams.update({"font.size": fontsize})
-    # # Generate a custom diverging colormap
-    # cmap = sns.diverging_palette(230, 20, as_cmap=True)
-    # plt.rcParams["text.usetex"] = True
-    # # Draw the heatmap with the mask and correct aspect ratio
-    # sns.heatmap(
-    #     corr,
-    #     mask=mask,
-    #     cmap=cmap,
-    #     vmin=0,
-    #     vmax=1,
-    #     center=0.5,
-    #     annot=True,
-    #     square=True,
-    #     linewidths=0.5,
-    #     cbar_kws={"shrink": 0.5},
-    # )
-    # # cbar_kws={"shrink": .5, "use_gridspec": False, "location": "top"})  #, annot_kws={"rotation": 45})
-    # plt.rcParams["text.usetex"] = False
-    # ax.set_xticklabels(weight_df.columns.to_list(), rotation=45, ha="right")
-    # plt.savefig("%s/Fig_3A.svg" % out_fig_dir, format="svg", bbox_inches="tight")
-    # plt.savefig("%s/Fig_3A.pdf" % out_fig_dir, format="pdf", bbox_inches="tight")
-    # plt.show()
-
     plot_corr_heatmap(weight_df, "%s/Fig_3A" % out_fig_dir, fontsize=12)
 
     lambdas = [0.0, 1.0, 2.0, 5.0, 8.0, 10.0]
@@ -202,9 +117,12 @@ def main():
         for group_label in outfile_dict[dataset].keys():
             weights = load_weights_dict(base_dir, dataset, group_label, lambdas)
             df = pd.DataFrame(weights)
-            plot_corr_heatmap(df, os.path.join(out_fig_dir, outfile_dict[dataset][group_label]), fontsize=14)
+            plot_corr_heatmap(
+                df,
+                os.path.join(out_fig_dir, outfile_dict[dataset][group_label]),
+                fontsize=14,
+            )
 
-            # load_coef_plot_corr(dataset, group_label, fontsize=fontsize)
 
 if __name__ == "__main__":
     main()
